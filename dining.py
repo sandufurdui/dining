18d34e96387a42ada93b26a204d33ba419e57a46
sandufurdui <76735601+sandufurdui@users.noreply.github.com>
2021-11-23 02:48:03 +0200

    Update dining.py

--- dining.py
+++ dining.py
@@ -1,17 +1,29 @@
-
 import socket
+import pickle
 
-HOST = 'localhost'  # Standard loopback interface address (localhost)
-PORT = 1500       # Port to listen on 1500
+HEADERSIZE = 30
+host = 'localhost'
+port = 4000
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host, port))
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print('Connection from ', addr , "(kitchen)")
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+while True:
+    full_msg = b''
+    new_msg = True
+    while True:
+        msg = s.recv(450)
+        if new_msg:
+            msglen = int(msg[:HEADERSIZE])
+            new_msg = False
+
+        print(f"full message length: {msglen}")
+
+        full_msg += msg
+
+        print(len(full_msg))
+
+        if len(full_msg)-HEADERSIZE == msglen:
+            print("full msg recvd")
+            print(pickle.loads(full_msg[HEADERSIZE:])) #decodes the message
+            new_msg = True
+            full_msg = b""
